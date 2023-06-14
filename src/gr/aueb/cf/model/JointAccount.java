d5dd993fa11096d4958e4b71620b5472173cf3c3
stavrosnaz <stavrosnaz@gmail.com>
2023-06-14 19:16:08 +0300

    v0.1

---
+++ src/gr/aueb/cf/model/JointAccount.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.model;
+
+public class JointAccount  extends Account {
+    private User secondHolder = new User();
+
+    public JointAccount() {}
+
+    public JointAccount(User holder, String iban, double balance, User secondHolder) {
+        super(holder, iban, balance);
+        this.secondHolder = secondHolder;
+    }
+
+    public User getSecondHolder() {
+        return secondHolder;
+    }
+
+    public void setSecondHolder(User secondHolder) {
+        this.secondHolder = secondHolder;
+    }
+
+    @Override
+    public String toString() {
+        return "JointAccount{" + getHolder() + "First holder: " +
+                "secondHolder= " + secondHolder + ", Iban: " + getIban() +
+                ", Balance: " + getBalance() +
+                '}';
+    }
+
+    @Override
+    protected boolean isSsnValid(String ssn) {
+        return super.isSsnValid(ssn) || secondHolder.getSsn().equals(ssn);
+    }
+
+
+}
